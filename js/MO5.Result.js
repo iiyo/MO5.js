9b9f1e2acf2104729237f4a12f4633129a002825
Jonathan Steinbeck <jonathan.steinbeck@caetec.de>
2014-07-18 15:24:46 +0200

    Changed MO5.Result to work with node.js.

--- js/MO5.Result.js
+++ js/MO5.Result.js
@@ -32,145 +32,179 @@
 
 /////////////////////////////////////////////////////////////////////////////////*/
 
-/* global MO5, window, setTimeout */
+/* global MO5, window, setTimeout, module, require, process */
 
-MO5("MO5.CoreObject", "MO5.Queue", "MO5.Exception", "MO5.fail").
-define("MO5.Result", function (CoreObject, Queue, Exception, fail) {
-    
-    var setImmediate = window.setImmediate || function (fn) { setTimeout(fn, 0); };
-    
-    function resolve (queue, value) {
-        
-        while (queue.hasNext()) {
-            setImmediate((function (cur) { 
-                return function () { 
-                    cur(value); 
-                };
-            }(queue.next())));
-        }
+(function MO5ResultBootstrap () {
+
+    if (typeof MO5 === "function") {
+        MO5("MO5.CoreObject", "MO5.Queue", "MO5.Exception", "MO5.fail").
+        define("MO5.Result", MO5ResultModule);
+    }
+    else if (typeof window !== "undefined") {
+        window.MO5.CoreObject = MO5ResultModule(
+            MO5.CoreObject,
+            MO5.Queue,
+            MO5.Exception,
+            MO5.fail
+        );
+    }
+    else {
+        module.exports = MO5ResultModule(
+            require("./MO5.CoreObject.js"),
+            require("./MO5.Queue.js"),
+            require("./MO5.Exception.js"),
+            require("./MO5.fail.js")
+        );
     }
     
-    function addToQueue (type, queue, cb, action) {
+    function MO5ResultModule (CoreObject, Queue, Exception, fail) {
+
+        var setImmediate;
         
-        if (typeof cb === "function") {
-            queue.add(function (value) {
-                
-                var nextValue;
-                
-                try {
-                    nextValue = cb(value);
-                    
-                    if (nextValue && nextValue instanceof Promise) {
-                        nextValue.then(action.success, action.failure);
-                    }
-                    else {
-                        action.success(nextValue);
-                    }
-                }
-                catch (e) {
-                    action.failure(e);
-                }
-                
-                return nextValue;
-            });
+        if (typeof window !== "undefined" && window.setImmediate) {
+            setImmediate = window.setImmediate;
+        }
+        else if (typeof process !== "undefined") {
+            setImmediate = function (fn) { process.nextTick(fn); };
         }
         else {
-            queue.add(function (value) { action[type](value); });
+            setImmediate = function (fn) { setTimeout(fn, 0); };
         }
-    }
-    
-    function Result () {
-        
-        CoreObject.call(this);
-        
-        this.successQueue = new Queue();
-        this.failureQueue = new Queue();
-        this.value = undefined;
-        this.status = Result.STATUS_PENDING;
-        
-        this.promise = new Promise(this);
-    }
-    
-    Result.STATUS_PENDING = 1;
-    Result.STATUS_FAILURE = 2;
-    Result.STATUS_SUCCESS = 3;
-    
-    Result.getFulfilledPromise = function () {
-        return new Result().success().promise;
-    };
-    
-    Result.getBrokenPromise = function () {
-        return new Result().failure().promise;
-    };
-    
-    Result.prototype = new CoreObject();
-    
-    Result.prototype.isPending = function () {
-        return this.status === Result.STATUS_PENDING;
-    };
-    
-    Result.prototype.failure = function (reason) {
-        if (this.status !== Result.STATUS_PENDING) {
-            fail(new Exception("The result of the action has already been determined."));
-            return;
-        }
-        
-        this.value = reason;
-        this.status = Result.STATUS_FAILURE;
-        resolve(this.failureQueue, reason);
-        this.successQueue.clear();
-        this.failureQueue.clear();
-        
-        return this;
-    };
-    
-    Result.prototype.success = function (value) {
-        if (this.status !== Result.STATUS_PENDING) {
-            fail(new Exception("The result of the action has already been determined."));
-            return;
-        }
-        
-        this.value = value;
-        this.status = Result.STATUS_SUCCESS;
-        resolve(this.successQueue, value);
-        this.successQueue.clear();
-        this.failureQueue.clear();
-        
-        return this;
-    };
-    
-    Result.addToQueue = addToQueue;
-    Result.resolve = resolve;
-    
-    function Promise (result) {
-        
-        CoreObject.call(this);
-        
-        this.then = function (success, failure) {
-            
-            var newResult = new Result();
-            
-            switch (result.status) {
-                case Result.STATUS_PENDING:
-                    Result.addToQueue("success", result.successQueue, success, newResult);
-                    Result.addToQueue("failure", result.failureQueue, failure, newResult);
-                    break;
-                case out.Result.STATUS_SUCCESS:
-                    out.Result.addToQueue("success", result.successQueue, success, newResult);
-                    out.Result.resolve(result.successQueue, result.value);
-                    break;
-                case out.Result.STATUS_FAILURE:
-                    out.Result.addToQueue("failure", result.failureQueue, failure, newResult);
-                    out.Result.resolve(result.failureQueue, result.value);
-                    break;
+
+        function resolve (queue, value) {
+
+            while (queue.hasNext()) {
+                setImmediate((function (cur) { 
+                    return function () { 
+                        cur(value); 
+                    };
+                }(queue.next())));
             }
-            
-            return newResult.promise;
+        }
+
+        function addToQueue (type, queue, cb, action) {
+
+            if (typeof cb === "function") {
+                queue.add(function (value) {
+
+                    var nextValue;
+
+                    try {
+                        nextValue = cb(value);
+
+                        if (nextValue && nextValue instanceof Promise) {
+                            nextValue.then(action.success, action.failure);
+                        }
+                        else {
+                            action.success(nextValue);
+                        }
+                    }
+                    catch (e) {
+                        action.failure(e);
+                    }
+
+                    return nextValue;
+                });
+            }
+            else {
+                queue.add(function (value) { action[type](value); });
+            }
+        }
+
+        function Result () {
+
+            CoreObject.call(this);
+
+            this.successQueue = new Queue();
+            this.failureQueue = new Queue();
+            this.value = undefined;
+            this.status = Result.STATUS_PENDING;
+
+            this.promise = new Promise(this);
+        }
+
+        Result.STATUS_PENDING = 1;
+        Result.STATUS_FAILURE = 2;
+        Result.STATUS_SUCCESS = 3;
+
+        Result.getFulfilledPromise = function () {
+            return new Result().success().promise;
         };
+
+        Result.getBrokenPromise = function () {
+            return new Result().failure().promise;
+        };
+
+        Result.prototype = new CoreObject();
+
+        Result.prototype.isPending = function () {
+            return this.status === Result.STATUS_PENDING;
+        };
+
+        Result.prototype.failure = function (reason) {
+            if (this.status !== Result.STATUS_PENDING) {
+                fail(new Exception("The result of the action has already been determined."));
+                return;
+            }
+
+            this.value = reason;
+            this.status = Result.STATUS_FAILURE;
+            resolve(this.failureQueue, reason);
+            this.successQueue.clear();
+            this.failureQueue.clear();
+
+            return this;
+        };
+
+        Result.prototype.success = function (value) {
+            if (this.status !== Result.STATUS_PENDING) {
+                fail(new Exception("The result of the action has already been determined."));
+                return;
+            }
+
+            this.value = value;
+            this.status = Result.STATUS_SUCCESS;
+            resolve(this.successQueue, value);
+            this.successQueue.clear();
+            this.failureQueue.clear();
+
+            return this;
+        };
+
+        Result.addToQueue = addToQueue;
+        Result.resolve = resolve;
+
+        function Promise (result) {
+
+            CoreObject.call(this);
+
+            this.then = function (success, failure) {
+
+                var newResult = new Result();
+
+                switch (result.status) {
+                    case Result.STATUS_PENDING:
+                        Result.addToQueue("success", result.successQueue, success, newResult);
+                        Result.addToQueue("failure", result.failureQueue, failure, newResult);
+                        break;
+                    case Result.STATUS_SUCCESS:
+                        Result.addToQueue("success", result.successQueue, success, newResult);
+                        Result.resolve(result.successQueue, result.value);
+                        break;
+                    case Result.STATUS_FAILURE:
+                        Result.addToQueue("failure", result.failureQueue, failure, newResult);
+                        Result.resolve(result.failureQueue, result.value);
+                        break;
+                }
+
+                return newResult.promise;
+            };
+        }
+
+        Promise.prototype = new CoreObject();
+
+        return Result;
+
     }
     
-    Promise.prototype = new CoreObject();
-    
-    return Result;
-    
-});
+}());
