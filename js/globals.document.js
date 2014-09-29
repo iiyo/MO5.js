ea15c4083743a11a026cce6650331c85f4a630e7
Jonathan Steinbeck <jonathan.steinbeck@caetec.de>
2014-09-29 10:34:53 +0200

    Added modules for DOM document and window globals to improve testability of browser stuff.

---
+++ js/globals.document.js
@@ -0,0 +1,5 @@
+/* global MO5, document */
+
+MO5().define("MO5.globals.document", function () {
+    return document;
+});
---
+++ js/globals.window.js
@@ -0,0 +1,5 @@
+/* global MO5, window */
+
+MO5().define("MO5.globals.window", function () {
+    return window;
+});
